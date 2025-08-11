46063ffbb6047b8343428bf8ceb06c3d3f5e39d8
daniel <danieldalati1111@gmail.com>
2025-08-11 21:38:05 +0300

    Initial commit

---
+++ src/test/java/testCases/TC_003.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class TC_003 {
+
+    @Test
+    public void t1(){
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver=new FirefoxDriver();
+        driver.get("https://facebook.com/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+}
